a196db66f6aaf6a085c638286b31004fc9894cd9
boral <boral@satsvyaz.ru>
2025-09-03 13:49:29 +0700

    Add GitHub Actions CI pipeline

--- AspNetCoreGitActions/Help/InitCommands.cs
+++ AspNetCoreGitActions/Help/InitCommands.cs
@@ -3,7 +3,7 @@
 // Install-Package Swashbuckle.AspNetCore
 // Install-Package Microsoft.AspNetCore.OpenApi -Version 8.0.8
 
-// In Visual Studio:
+// In Visual Studio добавить тесты:
 // File - Add - New Project
 // xUnit Test Project (.NET 8)
 // AspNetCoreGitActions.Tests
@@ -22,3 +22,8 @@
 // git remote add origin https://github.com/zhelezkino/AspNetCoreGitActions.git
 // git branch -M main
 // git push -u origin main
+
+// In Visual Studio настройка GitHub Actions:
+// Новый файл в проекте: .github/workflows/ci.yml
+// ProjectList - Add - New Folder - ".github" - "workflows"
+// "workflows" - Add - New Item - ci.yml 
